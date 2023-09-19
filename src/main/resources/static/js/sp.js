32a0c0f245ee6d6cdf30c7602be7becd348259ea
HANGOCHAN\HANGOCHAN <hanhnph24141@fpt.edu.vn>
2023-09-19 22:37:27 +0700

    add js

---
+++ src/main/resources/static/js/sp.js
@@ -0,0 +1,15 @@
+
+    let han = function (){
+        alert('okkk');
+    }
+
+    // let url = "http://localhost:8080/api/sanpham";
+    // let app = angular.module("myApp", []);
+    // app.controller("ctrl", function ($scope, $http){
+    //     $scope.list = [];
+    //     $http.get(url).then(function (response) {
+    //         $scope.list = response.data;
+    //     });
+    // });
+
+
