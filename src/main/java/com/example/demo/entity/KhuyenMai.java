c69b3103ad94400cd34809eab5704c9a6aff1f41
HUYNHPHUNG\Huynh <huynhphung2107@gmail.com>
2023-09-20 20:25:31 +0700

    update voucher

--- src/main/java/com/example/demo/entity/KhuyenMai.java
+++ src/main/java/com/example/demo/entity/KhuyenMai.java
@@ -17,28 +17,40 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class KhuyenMai implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     @Column(name = "IdKhuyenMai")
     private UUID IdKhuyenMai;
+
     @Column(name = "MaKhuyenMai")
     private String MaKhuyenMai;
+
     @Column(name = "TenKhuyenMai")
     private String TenKhuyenMai;
+
     @Column(name = "ChietKhau")
     private Integer ChietKhau;
+
     @Column(name = "NgayBatDau")
     private Date NgayBatDau;
+
     @Column(name = "NgayKetThuc")
     private Date NgayKetThuc;
+
     @Column(name = "NgayTao")
     private Date NgayTao;
+
     @Column(name = "NgayCapNhat")
     private Date NgayCapNhat;
+
     @Column(name = "NguoiCapNhat")
     private String NguoiCapNhat;
+
     @Column(name = "TrangThai")
     private Integer TrangThai;
+
     @JsonBackReference
     @OneToMany(mappedBy = "khuyenMai")
     private List<HoaDon> hoaDons;
