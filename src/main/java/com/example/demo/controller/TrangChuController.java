9916617972ddfcedfccb34130c0aa47edad94821
vietpham2k3 <vietvuavanvat13579@gmail.com>
2023-10-08 21:22:52 +0700

    cấu hình lại css cho cilent

---
+++ src/main/java/com/example/demo/controller/TrangChuController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/client/")
+public class TrangChuController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "/Client/home";
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+        return "/Client/detailSP";
+    }
+}
